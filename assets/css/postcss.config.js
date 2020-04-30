0b35fbe949e6011293d6e93e85f13b5ba059d289
Dirk Olbrich <mail@dirkolbrich.de>
2020-04-30 16:29:28 +0200

    refactor: clean out purgecss setup

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -1,28 +1,37 @@
 const themeDir = __dirname + '/../../';
 
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project
+    content: [
+        themeDir + 'layouts/**/*.html',
+        themeDir + 'exampleSite/content/**/*.html',
+        'layouts/**/*.html',
+        'content/**/*.html',
+    ],
+
+    // This is the function used to extract class names from your templates
+    defaultExtractor: content => {
+        // Capture as liberally as possible, including things like `h-(screen-1.5)`
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+        // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+
+        return broadMatches.concat(innerMatches)
+    }
+})
+
 module.exports = {    
     plugins: [        
         require('postcss-import')({
             path: [themeDir]
             }), 
         require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
-        // Configuration of purgecss for Tailwindcss
-        // see https://tailwindcss.com/docs/controlling-file-size/#setting-up-purgecss
-        require('@fullhuman/postcss-purgecss')({
-            // Specify the paths to all of the template files in your project 
-            content: [
-                themeDir + 'layouts/**/*.html',
-                themeDir + 'exampleSite/content/**/*.html',
-                'layouts/**/*.html',
-                'content/**/*.html',
-            ],
-            // Include any special characters you're using in this regular expression
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [], 
-            fontFace: true
-        }),    
         require('autoprefixer')({
+            path: [themeDir],
             grid: true
         }),
-        require('postcss-reporter'),
+        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
     ]
 }
