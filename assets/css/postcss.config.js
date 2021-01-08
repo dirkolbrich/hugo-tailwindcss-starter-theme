23ea9b12f76b2017452f916c3ebabf0938e2c807
Dirk Olbrich <mail@dirkolbrich.de>
2021-01-08 15:21:34 +0100

    refactor: change postcss-purgecss extractor

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -1,26 +1,15 @@
 const themeDir = __dirname + '/../../';
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
-
-    // Specify the paths to all of the template files in your project
+    // see https://gohugo.io/hugo-pipes/postprocess/#css-purging-with-postcss
     content: [
-        themeDir + 'layouts/**/*.html',
-        themeDir + 'content/**/*.html',
-        'layouts/**/*.html',
-        'content/**/*.html',
-        'exampleSite/layouts/**/*.html',
-        'exampleSite/content/**/*.html',
+        './hugo_stats.json',
+        themeDir + '/hugo_stats.json',
+        'exampleSite/hugo_stats.json',
     ],
-
-    // This is the function used to extract class names from your templates
-    defaultExtractor: content => {
-        // Capture as liberally as possible, including things like `h-(screen-1.5)`
-        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-
-        // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
-
-        return broadMatches.concat(innerMatches)
+    defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        return els.tags.concat(els.classes, els.ids);
     }
 })
 
