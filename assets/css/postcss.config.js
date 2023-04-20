c2e7571adf233cdeae4e680432ee2d918273753a
Dirk Olbrich <mail@dirkolbrich.de>
2023-04-20 17:17:01 +0200

    fix: color theme settings - respect system theme setting - add dark selector to purge safelist

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -7,7 +7,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
         themeDir + '/hugo_stats.json',
         'exampleSite/hugo_stats.json',
     ],
-    safelist : [ /type/ ],
+    safelist : [ /type/ , 'dark' ],
     defaultExtractor: (content) => {
         let els = JSON.parse(content).htmlElements;
         return els.tags.concat(els.classes, els.ids);
