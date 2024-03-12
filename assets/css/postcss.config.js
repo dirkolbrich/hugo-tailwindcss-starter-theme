2d3120efca061be212e4773bd3825a36679167c6
Dirk Olbrich <mail@dirkolbrich.de>
2024-03-12 17:24:00 +0100

    upgrade: first try on tailwindcss v4 import and build works, but css is totally messed up

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -1,10 +1,7 @@
 const themeDir = __dirname + '/../../';
 
 module.exports = {    
-    plugins: [
-        require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
-        require('autoprefixer')({
-            path: [themeDir]
-        })
-    ]
+    plugins: {
+        '@tailwindcss/postcss': {}
+    }
 }
